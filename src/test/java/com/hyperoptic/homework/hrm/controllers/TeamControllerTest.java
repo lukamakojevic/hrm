23cdc66d22cf4d2d09f39dd5551d1a1ef8b3a471
Luka Makojevic <76128919+lukamakojevic@users.noreply.github.com>
2023-11-08 00:37:12 +0100

    Adding more controller and service integraion tests (#15)

--- src/test/java/com/hyperoptic/homework/hrm/controllers/TeamControllerTest.java
+++ src/test/java/com/hyperoptic/homework/hrm/controllers/TeamControllerTest.java
@@ -5,6 +5,9 @@ import com.hyperoptic.homework.hrm.models.Team;
 import com.hyperoptic.homework.hrm.models.TeamSearchParams;
 import com.hyperoptic.homework.hrm.services.TeamService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,9 +20,12 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static com.hyperoptic.homework.hrm.TestUtils.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -30,27 +36,43 @@ class TeamControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
     @Autowired
     private ObjectMapper mapper;
-
 
     @MockBean
     private TeamService teamService;
 
     @Test
     void create() throws Exception {
-        URI uri = URI.create(BASE_PATH);
-
         Team team = team();
+
+        URI uri = URI.create(BASE_PATH);
 
         RequestBuilder request = MockMvcRequestBuilders.post(uri)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(mapper.writeValueAsString(team));
 
-        mockMvc.perform(request)
-                .andExpect(status().isCreated());
+        mockMvc.perform(request).andExpect(status().isCreated());
 
         verify(teamService).create(team);
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidNameTestCases")
+    void createWithInvalidName(String invalidName) throws Exception {
+        Team team = team();
+        team.setName(invalidName);
+
+        URI uri = URI.create(BASE_PATH);
+
+        RequestBuilder request = MockMvcRequestBuilders.post(uri)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(team));
+
+        mockMvc.perform(request).andExpect(status().isBadRequest());
+
+        verifyNoInteractions(teamService);
     }
 
     @Test
@@ -84,28 +106,45 @@ class TeamControllerTest {
 
         RequestBuilder request = MockMvcRequestBuilders.get(uri);
 
-        mockMvc.perform(request)
-                .andExpect(status().isOk());
+        mockMvc.perform(request).andExpect(status().isOk());
 
         verify(teamService).read(searchParams);
     }
 
     @Test
     void update() throws Exception {
+        Team team = team();
+
         URI uri = UriComponentsBuilder.fromPath(BASE_PATH + "/{id}")
                 .buildAndExpand(TEAM_ID)
                 .toUri();
-
-        Team team = team();
 
         RequestBuilder request = MockMvcRequestBuilders.put(uri)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(mapper.writeValueAsString(team));
 
-        mockMvc.perform(request)
-                .andExpect(status().isOk());
+        mockMvc.perform(request).andExpect(status().isOk());
 
         verify(teamService).update(TEAM_ID, team);
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidNameTestCases")
+    void updateWithInvalidName(String invalidName) throws Exception {
+        Team team = team();
+        team.setName(invalidName);
+
+        URI uri = UriComponentsBuilder.fromPath(BASE_PATH + "/{id}")
+                .buildAndExpand(TEAM_ID)
+                .toUri();
+
+        RequestBuilder request = MockMvcRequestBuilders.put(uri)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(team));
+
+        mockMvc.perform(request).andExpect(status().isBadRequest());
+
+        verifyNoInteractions(teamService);
     }
 
     @Test
@@ -120,5 +159,9 @@ class TeamControllerTest {
                 .andExpect(status().isOk());
 
         verify(teamService).delete(TEAM_ID);
+    }
+
+    static Stream<Arguments> invalidNameTestCases() {
+        return Stream.of(arguments((Object) null), arguments(" "), arguments(""));
     }
 }
---
+++ src/test/java/com/hyperoptic/homework/hrm/services/EmployeeServiceIntegrationTest.java
@@ -0,0 +1,168 @@
+package com.hyperoptic.homework.hrm.services;
+
+import com.hyperoptic.homework.hrm.entities.EmployeeEntity;
+import com.hyperoptic.homework.hrm.entities.TeamEntity;
+import com.hyperoptic.homework.hrm.exceptions.ExceptionSupplier;
+import com.hyperoptic.homework.hrm.exceptions.HrmException;
+import com.hyperoptic.homework.hrm.models.Employee;
+import com.hyperoptic.homework.hrm.repositories.EmployeeRepository;
+import com.hyperoptic.homework.hrm.repositories.TeamRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static com.hyperoptic.homework.hrm.TestUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class EmployeeServiceIntegrationTest {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private TeamRepository teamRepository;
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @BeforeEach
+    void setUp() {
+        employeeRepository.deleteAll();
+        teamRepository.deleteAll();
+    }
+
+    @Test
+    void create() {
+        TeamEntity teamEntity = teamRepository.save(teamEntity());
+
+        Employee employee = employee();
+        employee.setTeamId(teamEntity.getId());
+
+        Employee createdEmployee = employeeService.create(employee);
+
+        assertEquals(1, employeeRepository.count());
+
+        assertNotNull(createdEmployee.getId());
+        assertEquals(employee.getName(), createdEmployee.getName());
+        assertEquals(employee.getTeamId(), createdEmployee.getTeamId());
+    }
+
+    @Test
+    void createWithoutTeam() {
+        Employee employee = employee();
+        employee.setTeamId(null);
+
+        Employee createdEmployee = employeeService.create(employee);
+
+        assertEquals(1, employeeRepository.count());
+
+        assertNotNull(createdEmployee.getId());
+        assertEquals(employee.getName(), createdEmployee.getName());
+        assertNull(createdEmployee.getTeamId());
+    }
+
+    @Test
+    void createWithNonExistingTeam() {
+        Employee employee = employee();
+
+        HrmException exception = assertThrows(HrmException.class, () -> employeeService.create(employee));
+
+        assertEquals(ExceptionSupplier.TEAM_NOT_FOUND.get(), exception);
+    }
+
+    @Test
+    void read() {
+        TeamEntity teamEntity = teamRepository.save(teamEntity());
+        EmployeeEntity employeeEntity = employeeRepository.save(employeeEntity(teamEntity));
+
+        Employee readEmployee = employeeService.read(employeeEntity.getId());
+
+        assertEquals(employeeEntity.getId(), readEmployee.getId());
+        assertEquals(employeeEntity.getName(), readEmployee.getName());
+        assertEquals(employeeEntity.getTeam().getId(), readEmployee.getTeamId());
+    }
+
+    @Test
+    void readNonExistingEmployee() {
+        HrmException exception = assertThrows(HrmException.class, () -> employeeService.read(EMPLOYEE_ID));
+
+        assertEquals(ExceptionSupplier.EMPLOYEE_NOT_FOUND.get(), exception);
+    }
+
+    @Test
+    void update() {
+        TeamEntity teamEntity = teamRepository.save(teamEntity());
+        EmployeeEntity employeeEntity = employeeRepository.save(employeeEntity(teamEntity));
+
+        TeamEntity newTeamEntity = teamRepository.save(teamEntity());
+
+        Employee employeeUpdate = employee();
+        employeeUpdate.setName("New name");
+        employeeUpdate.setTeamId(newTeamEntity.getId());
+
+        Employee updatedEmployee = employeeService.update(employeeEntity.getId(), employeeUpdate);
+
+        assertEquals(employeeUpdate.getId(), updatedEmployee.getId());
+        assertEquals(employeeUpdate.getName(), updatedEmployee.getName());
+        assertEquals(employeeUpdate.getTeamId(), updatedEmployee.getTeamId());
+    }
+
+    @Test
+    void updateWithNoTeam() {
+        TeamEntity teamEntity = teamRepository.save(teamEntity());
+        EmployeeEntity employeeEntity = employeeRepository.save(employeeEntity(teamEntity));
+
+        Employee employeeUpdate = employee();
+        employeeUpdate.setName("New name");
+        employeeUpdate.setTeamId(null);
+
+        Employee updatedEmployee = employeeService.update(employeeEntity.getId(), employeeUpdate);
+
+        assertEquals(employeeUpdate.getId(), updatedEmployee.getId());
+        assertEquals(employeeUpdate.getName(), updatedEmployee.getName());
+        assertNull(updatedEmployee.getTeamId());
+    }
+
+    @Test
+    void updateNonExistingEmployee() {
+        Employee employeeUpdate = employee();
+
+        HrmException exception = assertThrows(HrmException.class, () -> employeeService.update(EMPLOYEE_ID, employeeUpdate));
+
+        assertEquals(ExceptionSupplier.EMPLOYEE_NOT_FOUND.get(), exception);
+    }
+
+    @Test
+    void updateWithNonExistingTeam() {
+        TeamEntity teamEntity = teamRepository.save(teamEntity());
+        EmployeeEntity employeeEntity = employeeRepository.save(employeeEntity(teamEntity));
+
+        Employee employeeUpdate = employee();
+        employeeUpdate.setName("New name");
+        employeeUpdate.setTeamId(99999);
+
+        HrmException exception = assertThrows(HrmException.class, () -> employeeService.update(employeeEntity.getId(), employeeUpdate));
+
+        assertEquals(ExceptionSupplier.TEAM_NOT_FOUND.get(), exception);
+    }
+
+    @Test
+    void delete() {
+        EmployeeEntity employeeEntity = employeeRepository.save(employeeEntity());
+
+        assertTrue(employeeRepository.findById(employeeEntity.getId()).isPresent());
+
+        employeeService.delete(employeeEntity.getId());
+
+        assertFalse(employeeRepository.findById(employeeEntity.getId()).isPresent());
+    }
+
+    @Test
+    void deleteNonExistingEmployee() {
+        HrmException exception = assertThrows(HrmException.class, () -> employeeService.delete(EMPLOYEE_ID));
+
+        assertEquals(ExceptionSupplier.EMPLOYEE_NOT_FOUND.get(), exception);
+    }
+}
