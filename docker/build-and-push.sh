25d4f77bd2d0d9c8804c1a225230975bfe959b64
Luka Makojevic <76128919+lukamakojevic@users.noreply.github.com>
2024-04-14 03:02:25 +0200

    Update build-and-push.sh

--- docker/build-and-push.sh
+++ docker/build-and-push.sh
@@ -4,6 +4,6 @@ cd ..
 
 docker build -t lukamakojevic/hrm:arm64 -f ./docker/Dockerfile .
 
-echo dckr_pat_B6em4NipK0c4O348rPaVzaidrFI | docker login -u lukamakojevic --password-stdin
+echo dckr_pat_HRy-1JU_iEZsHLcf5idhEzMmSfE | docker login -u lukamakojevic --password-stdin
 
 docker push lukamakojevic/hrm:arm64
